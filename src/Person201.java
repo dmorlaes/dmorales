a6ba0c517533bcba23ca87ee8783b09783111f14
Owen Astrachan <ola@cs.duke.edu>
2020-08-09 21:22:39 -0400

    updated with scanner

--- src/Person201.java
+++ src/Person201.java
@@ -71,14 +71,4 @@ public class Person201 {
         return String.format("%s %s @ %s %s",myName,myPhrase,lats,lons);
     }
 
-    public static void main(String[] args) {
-        Person201 p = new Person201();
-        Person201 q =
-                new Person201("Ricardo",-34.6037, -58.3816,"harambee");
-        Person201 r =
-                new Person201("Gelareh",-33.89,151.2,"affective");
-        System.out.println(p);
-        System.out.println(q);
-        System.out.println(r);
-    }
 }
