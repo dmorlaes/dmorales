7adda6b8d1c2db5524bec9c7b39eeb6db16721f2
Dmitri Morales <dmitri.morales@duke.edu>
2020-08-29 10:32:06 -0400

    Added s to the Person201Driver

--- src/Person201Driver.java
+++ src/Person201Driver.java
@@ -5,10 +5,13 @@ public class Person201Driver {
                 new Person201("Ricardo",-34.6037, -58.3816,"harambee");
         Person201 r =
                 new Person201("Gelareh",-33.89,151.2,"affective");
+        Person201 s =
+                new Person201 ("Sam", 44.9978, -93.2650, "hello");
 
         System.out.println(p);
         System.out.println(q);
         System.out.println(r);
+        System.out.println(s);
 
         System.out.printf("%s %s\n",q.getName(), q.getPhrase());
         System.out.printf("%s %s\n",r.getName(), r.getPhrase());
